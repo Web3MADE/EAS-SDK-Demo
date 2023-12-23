c35b6333959438e9cd8abebd44502ac04e744165
Hakeem <mwo248@yahoo.com>
2023-12-23 12:46:12 +0000

    config setup

---
+++ Freelance.md
@@ -0,0 +1,15 @@
+# Freelance Platform
+
+This app will demonstrate how a truly decentralized platform for freelancers can work, using the Ethereum Attestation Service.
+
+## Part 1: EAS SDK
+
+This will show how to setup the eas SDK and create attestations for the Freelancer's reputation.
+
+## Part 2: Resolver
+
+This will show how to use resolvers for:
+
+1. Whitelisting addresses (only client can attest)
+2. Paying the Freelancer on successful attestation
+3. Refunding client on unsuccessful attestation
---
+++ src/config/eas-sdk.ts
@@ -0,0 +1,14 @@
+import { EAS } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from "ethers";
+
+export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+
+// Initialize the sdk with the address of the EAS Schema contract address
+const eas = new EAS(EASContractAddress);
+
+// Gets a default provider (in production use something else like infura/alchemy)
+const provider = ethers.getDefaultProvider("sepolia");
+
+// Connects an ethers style provider/signingProvider to perform read/write functions.
+// MUST be a signer to do write operations!
+eas.connect(provider);
